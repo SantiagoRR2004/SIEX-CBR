1298df42c6bcb08c6e834aa715ef39e187443931
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-11-16 18:46:13 +0100

    Added HTML file for redirecting to documentation and did formatting

--- valorador.py
+++ valorador.py
@@ -2,11 +2,15 @@ from core import CBR, CBR_DEBUG
 
 import cbrkit
 
+
 class Valorador(CBR):
-    def __init__(self, base_de_casos,
-                num_casos_similares = 100, 
-                taxonomia = "./datos/taxonomia.json",
-                debug=False):
+    def __init__(
+        self,
+        base_de_casos,
+        num_casos_similares=100,
+        taxonomia="./datos/taxonomia.json",
+        debug=False,
+    ):
         super().__init__(base_de_casos, num_casos_similares)
         if debug:
             self.debug = CBR_DEBUG(self.prettyprint_caso)
@@ -18,18 +22,18 @@ class Valorador(CBR):
         model_similarity = cbrkit.sim.attribute_value(
             attributes={
                 "assigner": cbrkit.sim.strings.jaro()
-                #añadir aquí otros atributos
+                # añadir aquí otros atributos
             },
-            aggregator=cbrkit.sim.aggregator(pooling="mean")
-            )
-        
-        #añadir aquí otros modelos de similitud
+            aggregator=cbrkit.sim.aggregator(pooling="mean"),
+        )
+
+        # añadir aquí otros modelos de similitud
         case_similarity = cbrkit.sim.attribute_value(
             attributes={
                 "model": model_similarity,
             },
-            aggregator=cbrkit.sim.aggregator(pooling="mean")
-            )
+            aggregator=cbrkit.sim.aggregator(pooling="mean"),
+        )
         retriever = cbrkit.retrieval.build(case_similarity, limit=num_casos_similares)
         return retriever
 
@@ -37,16 +41,19 @@ class Valorador(CBR):
         return self.retriever.similaridad(caso_a, caso_b)
 
     def recuperar(self, caso_a_resolver):
-        result = cbrkit.retrieval.apply(self.base_de_casos, caso_a_resolver, self.retriever)
+        result = cbrkit.retrieval.apply(
+            self.base_de_casos, caso_a_resolver, self.retriever
+        )
         casos_similares = []
         similaridades = []
         for i in result.ranking:
             casos_similares.append(self.base_de_casos[i])
             similaridades.append(result.similarities[i].value)
-        
+
         # DEBUG
-        if self.DEBUG : self.DEBUG.debug_recuperar(caso_a_resolver, casos_similares, similaridades)
-          
+        if self.DEBUG:
+            self.DEBUG.debug_recuperar(caso_a_resolver, casos_similares, similaridades)
+
         return (casos_similares, similaridades)
 
     """
@@ -64,13 +71,26 @@ class Valorador(CBR):
                 scores = [score for score in scores if score <= self.similaridad_max]
             return similares[scores.index(max(scores))] # Retorna el caso similar con mayor similitud
     """
+
     def reutilizar(self, caso_a_resolver, casos_similares, similaridades):
         pass
 
-    def revisar(self, caso_resuelto, caso_a_resolver=None, casos_similares=None, similaridades=None):
+    def revisar(
+        self,
+        caso_resuelto,
+        caso_a_resolver=None,
+        casos_similares=None,
+        similaridades=None,
+    ):
         pass
 
-    def retener(self, caso_revisado, caso_a_resolver=None, casos_similares=None, similaridades=None):
+    def retener(
+        self,
+        caso_revisado,
+        caso_a_resolver=None,
+        casos_similares=None,
+        similaridades=None,
+    ):
         pass
 
     def prettyprint_caso(self, caso):
