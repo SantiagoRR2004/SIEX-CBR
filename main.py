1d707fc7b63e5257a422109813b296f9b26deb11
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2024-11-19 21:05:10 +0100

    Added confusion matrix

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+from typing import List
 import cbrkit
 import argparse
 import pprint
 import random
 from valorador import Valorador
+
+
+def showConfusionMatrix(trueLabels: List[str], predictedLabels: List[str]) -> None:
+    """
+    Display a confusion matrix for the true and predicted labels.
+
+    The labels are assumed to be one of the following:
+        - NETWORK
+        - ADJACENT NETWORK
+        - LOCAL
+
+    Args:
+        - trueLabels (List[str]): List of true labels.
+        - predictedLabels (List[str]): List of predicted labels.
+
+    Returns:
+        - None
+    """
+    # Compute confusion matrix
+    cm = confusion_matrix(
+        trueLabels, predictedLabels, labels=["NETWORK", "ADJACENT NETWORK", "LOCAL"]
+    )
+
+    # Display confusion matrix
+    disp = ConfusionMatrixDisplay(
+        confusion_matrix=cm, display_labels=["NETWORK", "ADJACENT NETWORK", "LOCAL"]
+    )
+    disp.plot(cmap="Blues")
+    plt.show()
+
 
 if __name__ == "__main__":
     base_casos = cbrkit.loaders.json("./datos/base_casos.json")
@@ -10,6 +43,8 @@ if __name__ == "__main__":
     casos_a_resolver = cbrkit.loaders.json("./datos/casos_a_resolver.json")
 
     contador_exitos = 0
+    realAV = []
+    predictedAV = []
 
     for caso in casos_a_resolver.values():
         caso_resuelto = valorador.ciclo_cbr(caso)
@@ -21,5 +56,9 @@ if __name__ == "__main__":
         print(f"Attack vector predicho: {caso['_meta']['attack_vector_predicho']}")
         print(f"Attack vector real: {caso_resuelto['_meta']['attack_vector_real']}")
         print("---------------------")
+        realAV.append(caso_resuelto["_meta"]["attack_vector_real"])
+        predictedAV.append(caso["_meta"]["attack_vector_predicho"])
 
-    print(f"Númeor de casos exitosos: {contador_exitos} de {len(casos_a_resolver)}")
+    print(f"Número de casos exitosos: {contador_exitos} de {len(casos_a_resolver)}")
+
+    showConfusionMatrix(realAV, predictedAV)
